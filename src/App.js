ecb2d6d2a5bc18c470a448e720a15ce1467d1063
“Onur” <onurpolat711@gmail.com>
2021-11-18 12:38:28 +0300

    Feat: Router created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Login from './pages/auth/Login/Login';
+import Sign from './pages/auth/Sign/Sign';
+import Messages from './pages/Messages/Messages';
+const Stack = createNativeStackNavigator();
 export default function App() {
+  const AuthStack = () => {
+    return (
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Sign" component={Sign} />
+      </Stack.Navigator>
+    );
+  };
   return (
-    <View>
-      <Text>RNFirebase</Text>
-    </View>
-  )
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="AuthStack" component={AuthStack} />
+        <Stack.Screen name="Messages" component={Messages} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
---
+++ src/pages/auth/Sign/Sign.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Sign() {
+    return (
+        <View>
+            <Text>Sign</Text>
+        </View>
+    )
+}
