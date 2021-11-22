099371f38280b90ba3cc32d683e8043a8fb9a9f2
“Onur” <onurpolat711@gmail.com>
2021-11-22 17:40:17 +0300

    Feat: LogOut button added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Login from './pages/auth/Login';
 import Sign from './pages/auth/Sign';
 import Messages from './pages/Messages';
-import FlashMessage from "react-native-flash-message";
-import { colors } from './configs/Colors';
+import FlashMessage from 'react-native-flash-message';
+import auth from '@react-native-firebase/auth';
+import {colors} from './configs/Colors';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 const Stack = createNativeStackNavigator();
 export default function App() {
+  const [userSession, setUserSession] = useState();
+  useEffect(() => {
+    auth().onAuthStateChanged(user => {
+      setUserSession(!!user);
+    });
+  }, []);
   const AuthStack = () => {
     return (
       <Stack.Navigator screenOptions={{headerShown: false}}>
@@ -18,9 +26,24 @@ export default function App() {
   };
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}  >
-        <Stack.Screen name="AuthStack" component={AuthStack} />
-        <Stack.Screen name="Messages" component={Messages}  />
+      <Stack.Navigator>
+        {!userSession ? (
+          <Stack.Screen
+            name="AuthStack"
+            component={AuthStack}
+            options={{headerShown: false}}
+          />
+        ) : (
+          <Stack.Screen
+            name="Messages"
+            component={Messages}
+            options={{
+              title: 'dertler',
+              headerTintColor: colors.green,
+              headerRight: () => <Icon name="logout" size={30} color={colors.green} onPress={()=> auth().signOut()} />,
+            }}
+          />
+        )}
       </Stack.Navigator>
       <FlashMessage position="top" />
     </NavigationContainer>
